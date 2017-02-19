677ba98c409fb12201351f41f2f020094eccaebf
Fsyou7 <fsyoung7@gmail.com>
2017-02-19 10:18:02 -0800

    Initial commit

---
+++ app/src/main/java/com/labartbeats/lakemerritt/CategoryAdapter.java
@@ -0,0 +1,70 @@
+package com.labartbeats.lakemerritt;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * Created by Marc on 12/31/2016.
+ */
+
+
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Item} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /** Context of the app */
+    private Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new RecreationFragment();
+        } else if (position == 1){
+            return new FoodFragment();
+        } else if (position == 2){
+            return new WorkFragment();
+        } else {
+            return new RetailFragment();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.recreation_fragment);
+        } else if (position == 1){
+            return mContext.getString(R.string.food_fragment);
+        } else if (position == 2){
+            return mContext.getString(R.string.work_fragment);
+        } else {
+            return mContext.getString(R.string.retail_fragment);
+        }
+    }
+}
